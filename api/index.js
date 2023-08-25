6a3bc98e32cf4f1dc121a4125c1e2921bbd7be0b
UserSampath <nalakasampathsmp@gmail.com>
2023-08-26 00:15:44 +0530

    backend init

---
+++ api/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
+const app = express()
+const port = 8000;
+const cors=require('cors')
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+app.use(passport.initialize());
+const jwt = require('jsonwebtoken');
+
+
+mongoose.connect("mongodb+srv://sampath:sampath@cluster0.vtnflrx.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+   console.log("Connected to database " ) 
+}).catch((err) => {
+    console.log("Error connecting to db")
+})
+
+app.listen(port, () => {
+    console.log("server running on port " + port)
+});
