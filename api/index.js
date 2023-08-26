fed785914ad9fc20702b510d6e68f4d38d85e372
UserSampath <nalakasampathsmp@gmail.com>
2023-08-26 19:08:21 +0530

    add backend models

--- api/index.js
+++ api/index.js
@@ -6,10 +6,10 @@ const LocalStrategy = require('passport-local').Strategy
 
 const app = express()
 const port = 8000;
-const cors=require('cors')
+const cors = require('cors')
 app.use(cors());
 
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(passport.initialize());
 const jwt = require('jsonwebtoken');
@@ -19,7 +19,7 @@ mongoose.connect("mongodb+srv://sampath:sampath@cluster0.vtnflrx.mongodb.net/", 
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-   console.log("Connected to database " ) 
+    console.log("Connected to database ")
 }).catch((err) => {
     console.log("Error connecting to db")
 })
@@ -27,3 +27,8 @@ mongoose.connect("mongodb+srv://sampath:sampath@cluster0.vtnflrx.mongodb.net/", 
 app.listen(port, () => {
     console.log("server running on port " + port)
 });
+
+
+const User = require("./models/user");
+const Message = require("./models/message")
+
